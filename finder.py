73b27cba2c00ce4b98af89df657f7e1747ff13b5
Josue Barreno <maharba.josue@gmail.com>
2022-08-09 11:26:03 -0400

    fix a bad pull origin

--- finder.py
+++ finder.py
@@ -4,14 +4,15 @@
 from web3 import Web3, HTTPProvider
 
 #instantiate a web3 remote provider
+# other provider => w3 = Web3(HTTPProvider('http://18.229.97.105/'))
 w3 = Web3(HTTPProvider('http://3.128.144.155:23001'))
 
 #request the latest block number
-ending_blocknumber1 = w3.eth.blockNumber
-ending_blocknumber = 12900673
+# how request the last block number => ending_blocknumber = w3.eth.blockNumber
+#ending_blocknumber = 12900923
 
 #latest block number minus 100 blocks
-starting_blocknumber = 12620050
+#starting_blocknumber = 12840923
 
 #filter through blocks and look for transactions involving this address
 #blockchain_address = "0x5d9d830d89e260e733f073c4d80ff58caf3ac6bb"
@@ -26,19 +27,17 @@ def getTransactions(start, end):
     Additionally, it will write the found transactions to a pickle file for quickly serializing and de-serializing
     a Python object.'''
 
-    print(f"Started filtering through block number {start} to {end}")
+    #print(f"Started filtering through block number {start} to {end} for transactions involving the address - {address}...")
+    print(f"The last block generated is {w3.eth.blockNumber}")
+    print(f"Wait until generated the file output.txt...")
     for x in range(start, end):
-        print(f"Checking block number {x}")
         block = w3.eth.getBlock(x, True)
         for transaction in block.transactions:
-            a = str(transaction["from"])
-            b = str(transaction["to"])
-            #h = transaction["hash"]
-            #c = str(address)
-
-            
+            a = transaction["from"]
+            b = transaction["to"]
+            # h = transaction["hash"]
+            # c = str(address)
             print(f"{x} {a} {b}",file = f)
-
             # if transaction['to'] == address or transaction['from'] == address:
             #     with open("transactions.pkl", "wb") as f:
             #         hashStr = transaction['hash'].hex()
@@ -46,7 +45,14 @@ def getTransactions(start, end):
             #         pickle.dump(tx_dictionary, f)
             #     f.close()
     #print(f"Finished searching blocks {start} through {end} and found {len(tx_dictionary)} transactions")
+    print(f"Finished searching blocks {start} through {end} and create a list of transactions...")
 
-f = open ("output.txt","a")
-getTransactions(starting_blocknumber, ending_blocknumber)
-f.close()
+
+if __name__ == "__main__":
+    print(f"Inicializing finder.py ......")
+    starting_blocknumber = int(input("Find transactions from block number: "))
+    ending_blocknumber = int(input("Until block number: "))
+    f = open ("output.txt","a")
+    # getTransactions(starting_blocknumber, ending_blocknumber, blockchain_address)
+    getTransactions(starting_blocknumber, ending_blocknumber)
+    f.close()
